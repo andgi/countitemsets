16faa09d16288a9fb79345eab03b191635f8e7a8
HSU <HSU@ebfeac0a-93c6-ac4a-a35f-0c39878b68e0>
2014-09-29 10:10:05 +0000

    git-svn-id: https://non-blocking.com:8443/svn/HB/Forskning/FBI@789 ebfeac0a-93c6-ac4a-a35f-0c39878b68e0

---
+++ Programs/CountItemSets/Form1.Designer.cs
@@ -0,0 +1,128 @@
+﻿namespace CountItemSets
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBoxFileName = new System.Windows.Forms.TextBox();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.browseButton1 = new System.Windows.Forms.Button();
+            this.buttonStart = new System.Windows.Forms.Button();
+            this.dataGridViewResults = new System.Windows.Forms.DataGridView();
+            this.label2 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewResults)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(14, 16);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(52, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Filename:";
+            // 
+            // textBoxFileName
+            // 
+            this.textBoxFileName.Location = new System.Drawing.Point(69, 13);
+            this.textBoxFileName.Name = "textBoxFileName";
+            this.textBoxFileName.Size = new System.Drawing.Size(554, 20);
+            this.textBoxFileName.TabIndex = 1;
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // browseButton1
+            // 
+            this.browseButton1.Location = new System.Drawing.Point(629, 12);
+            this.browseButton1.Name = "browseButton1";
+            this.browseButton1.Size = new System.Drawing.Size(75, 23);
+            this.browseButton1.TabIndex = 2;
+            this.browseButton1.Text = "Browse...";
+            this.browseButton1.UseVisualStyleBackColor = true;
+            this.browseButton1.Click += new System.EventHandler(this.browseButton1_Click);
+            // 
+            // buttonStart
+            // 
+            this.buttonStart.Location = new System.Drawing.Point(69, 52);
+            this.buttonStart.Name = "buttonStart";
+            this.buttonStart.Size = new System.Drawing.Size(75, 23);
+            this.buttonStart.TabIndex = 3;
+            this.buttonStart.Text = "START";
+            this.buttonStart.UseVisualStyleBackColor = true;
+            this.buttonStart.Click += new System.EventHandler(this.buttonStart_Click);
+            // 
+            // dataGridViewResults
+            // 
+            this.dataGridViewResults.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridViewResults.Location = new System.Drawing.Point(69, 100);
+            this.dataGridViewResults.Name = "dataGridViewResults";
+            this.dataGridViewResults.Size = new System.Drawing.Size(635, 420);
+            this.dataGridViewResults.TabIndex = 4;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(13, 100);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(45, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Results:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(716, 532);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.dataGridViewResults);
+            this.Controls.Add(this.buttonStart);
+            this.Controls.Add(this.browseButton1);
+            this.Controls.Add(this.textBoxFileName);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "CountItemSets";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewResults)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBoxFileName;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button browseButton1;
+        private System.Windows.Forms.Button buttonStart;
+        private System.Windows.Forms.DataGridView dataGridViewResults;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ Programs/CountItemSets/Form1.cs
@@ -0,0 +1,199 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using Microsoft.VisualBasic.FileIO;
+
+namespace CountItemSets
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void browseButton1_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.ShowDialog();
+            textBoxFileName.Text = openFileDialog1.FileName; 
+
+        }
+
+        Dictionary<long, string> dictionaryEAN = new Dictionary<long, string>();
+        Dictionary<int, string> dictionaryVGR = new Dictionary<int, string>();
+        Dictionary<long, int> dictionaryEANtoVGR = new Dictionary<long, int>();
+
+        private void buttonStart_Click(object sender, EventArgs e)
+        {
+            buttonStart.Enabled = false;
+
+            TextFieldParser parser = new TextFieldParser("C:\\Users\\HSU\\EAN.csv");
+            parser.SetDelimiters(";");
+            parser.ReadLine();
+            while (!parser.EndOfData)
+            {
+                try
+                {
+                    string[] fields = parser.ReadFields();
+                    long eanNr = Int64.Parse(fields[0]);
+                    string eanDesc = fields[1];
+                    dictionaryEAN.Add(eanNr, eanDesc);
+                }
+                catch (Exception) { }
+            }
+
+            parser = new TextFieldParser("C:\\Users\\HSU\\VGR.csv");
+            parser.SetDelimiters(";");
+            parser.ReadLine();
+            while (!parser.EndOfData)
+            {
+                try
+                {
+                    string[] fields = parser.ReadFields();
+                    int vgrNr = Int32.Parse(fields[0]);
+                    string vgrDesc = fields[1];
+                    dictionaryVGR.Add(vgrNr, vgrDesc);
+                }
+                catch (Exception) { }
+            }
+
+
+
+            StreamReader reader = new StreamReader(textBoxFileName.Text);
+            int rowCount = 0;
+            int transNrLast = 0;
+            Dictionary<long,int> dictionary = new Dictionary<long,int>();
+            while (!reader.EndOfStream)
+            {
+                try
+                {
+                    String line = reader.ReadLine();
+                    String[] columns = line.Split(';');
+                    if (rowCount > 0)
+                    {
+                        int transNr = Int32.Parse(columns[0]);
+                        long eanNr = Int64.Parse(columns[1]);
+                        int vgrNr = Int32.Parse(columns[2]);
+                        if (transNrLast == 0)
+                        {
+                            transNrLast = transNr;
+                        }
+                        if (dictionary.ContainsKey(eanNr))
+                            dictionary[eanNr]++;
+                        else
+                            dictionary.Add(eanNr, 1);
+                        if (transNrLast != transNr)
+                        {
+                        }
+                        if (!dictionaryEANtoVGR.ContainsKey(eanNr))
+                            dictionaryEANtoVGR.Add(eanNr, vgrNr);
+                    }
+                    rowCount++;
+                }
+                catch (Exception ex) {
+                    MessageBox.Show(ex.Message);
+                };
+            }
+            dictionary = dictionary.Where(item => item.Value >= 1000 && item.Key!=1 && item.Key!=2).ToDictionary(item => item.Key, item => item.Value);
+
+
+            Dictionary<string, int> dictionary2 = new Dictionary<string, int>();
+
+            reader.Close();
+            reader = new StreamReader(textBoxFileName.Text);
+            transNrLast = 0;
+            rowCount = 0;
+            List<long> keys = new List<long>(10);
+            while (!reader.EndOfStream)
+            {
+                try
+                {
+                    String line = reader.ReadLine();
+                    String[] columns = line.Split(';');
+                    if (rowCount > 0)
+                    {
+                        int transNr = Int32.Parse(columns[0]);
+                        long eanNr = Int64.Parse(columns[1]);
+                        int vgrNr = Int32.Parse(columns[2]);
+                        if (transNrLast == 0)
+                        {
+                            transNrLast = transNr;
+                        }
+                        if (transNrLast != transNr)
+                        {
+                            keys.Sort();
+                            for (int i = 0; i < (keys.Count-1); i++)
+                            {
+                                for (int j = i + 1; j < keys.Count; j++)
+                                {
+                                    long key1 = keys[i];
+                                    long key2 = keys[j];
+                                    if (dictionary.ContainsKey(key1) && dictionary.ContainsKey(key2) && key1!=key2)
+                                    {
+                                        string keyName = key1 + "," + key2;
+                                        if (dictionary2.ContainsKey(keyName))
+                                            dictionary2[keyName]++;
+                                        else
+                                            dictionary2.Add(keyName, 1);
+                                    }
+                                }
+                            }
+                            keys.Clear();
+                            transNrLast = transNr;
+                        }
+                        keys.Add(eanNr);
+                    }
+                    rowCount++;
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                };
+            }
+
+            dictionary2 = dictionary2.Where(item => item.Value >= 1000).ToDictionary(item => TranslateEANpairs(item.Key), item => item.Value);
+
+            List<KeyValuePair<string,int>> results = dictionary2.OrderByDescending(item => item.Value).ToList();
+
+            dataGridViewResults.DataSource = results;
+
+            buttonStart.Enabled = true;
+
+        }
+
+        private string TranslateEANpairs(string textPair)
+        {
+            String[] columns = textPair.Split(',');
+            List<string> results = new List<string>();
+            foreach (string field in columns)
+            {
+                string result = field;
+                long eanNr = 0;
+                Int64.TryParse(field, out eanNr);
+                if (dictionaryEAN.ContainsKey(eanNr))
+                    result = dictionaryEAN[eanNr] + " (" + eanNr + ")";
+                else
+                {
+                    if (dictionaryEANtoVGR.ContainsKey(eanNr))
+                    {
+                        int vgrNr = dictionaryEANtoVGR[eanNr];
+                        if(dictionaryVGR.ContainsKey(vgrNr))
+                            result = dictionaryVGR[vgrNr] + " (" + eanNr + ")";
+                        else
+                            result = "Varugrupp " + vgrNr + " (" + eanNr + ")";
+                    }
+                }
+                results.Add(result);
+            }
+            return String.Join(",",results.ToArray());
+        }
+
+    }
+}
