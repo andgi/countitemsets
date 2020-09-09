0dd63d732ca431138a204afb67ed866f0fb93bc6
Anders Gidenstam <anders.gidenstam@hb.se>
2020-09-09 15:24:40 +0200

    Small improvement of the unit-tests for the OpenCL-based GPU variant.

--- Programs/OpenCLTests/GPUTransactionFrequentItemsetGeneratorTests.cs
+++ Programs/OpenCLTests/GPUTransactionFrequentItemsetGeneratorTests.cs
@@ -31,12 +31,47 @@ namespace OpenCLTests
             Assert.AreEqual<int>(reader.NoTransactions,
                                  unitUnderTest.GetNoTransactions());
             IDictionary<long, int> result1tuples = unitUnderTest.Get1TupleResult(computeCmdQueue);
-            CollectionAssert.AreEquivalent(reader.AllEANs.ToArray(), result1tuples.Keys.ToArray());
-            CollectionAssert.AreEquivalent(reader.EANFrequencies.ToArray(), result1tuples.ToArray());
+            CollectionAssert.AreEquivalent(reader.AllEANs.ToArray(),
+                                           result1tuples.Keys.ToArray(),
+                                           "The set of EANs is wrong.");
+            CollectionAssert.AreEquivalent(reader.EANFrequencies.ToArray(),
+                                           result1tuples.ToArray(),
+                                           "The set of EAN 1-tuple frequencies is wrong.");
             IDictionary<string, int> result2tuples = unitUnderTest.GetNTupleResult(2, computeCmdQueue);
-            CollectionAssert.AreEquivalent(reader.EAN2TupleFrequencies.ToArray(), result2tuples.ToArray());
+            var difference2tuple = reader.EAN2TupleFrequencies.Where(kv => !result2tuples.Keys.Contains(kv.Key));
+            foreach (var kv in difference2tuple)
+            {
+                uint hash = ComputeHash(kv);
+                System.Console.WriteLine("Missing in result2tuples: " + kv.Key + ": " + kv.Value +
+                                         " hash value = " + hash);
+            }
+            foreach (var kv in reader.EAN2TupleFrequencies.OrderBy(kv => uint.Parse(kv.Key.Split(',')[0]))) {
+                uint hash = ComputeHash(kv);
+                System.Console.WriteLine("  EAN2TupleFrequencies: " + kv.Key + ": " + kv.Value +
+                                         " hash value = " + hash);
+            }
+            CollectionAssert.AreEquivalent(reader.EAN2TupleFrequencies.ToArray(),
+                                           result2tuples.ToArray(),
+                                           "The set of EAN 2-tuple frequencies is wrong.");
             IDictionary<string, int> result3tuples = unitUnderTest.GetNTupleResult(3, computeCmdQueue);
-            CollectionAssert.AreEquivalent(reader.EAN3TupleFrequencies.ToArray(), result3tuples.ToArray());
+            CollectionAssert.AreEquivalent(reader.EAN3TupleFrequencies.ToArray(),
+                                           result3tuples.ToArray(),
+                                           "The set of EAN 3-tuple frequencies is wrong.");
+        }
+
+        /// <summary>
+        ///  Compute the hash, hopefully the same as the GPU code, but nothing depends on that.
+        ///  The hash value is just printed from the test here.
+        /// </summary>
+        /// <param name="kv"></param>
+        /// <returns></returns>
+        private static uint ComputeHash(KeyValuePair<string, int> kv)
+        {
+            uint key1 = uint.Parse(kv.Key.Split(',')[0]);
+            uint key2 = uint.Parse(kv.Key.Split(',')[1]);
+            //uint hash = (2 + 1) * ((key1 + (key2<<16)) % 8388607);
+            uint hash = (2 + 1) * ((((1 + key1 + key2) ^ 628346 + 94967291) % 4294967291) % 16777215);
+            return hash;
         }
 
         private GPUTransactionFrequentItemsetGenerator.GPUAlgorithm unitUnderTest;
